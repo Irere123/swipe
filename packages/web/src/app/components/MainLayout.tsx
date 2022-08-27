b7498994e2affdecd830ae93ffce2732eb4fb177
irere123 <irere2020@outlook.com>
2022-08-27 20:41:36 +0200

    feat(web): header component done

--- packages/web/src/app/components/MainLayout.tsx
+++ packages/web/src/app/components/MainLayout.tsx
@@ -1,5 +1,8 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Codicon, HomeIcon, MessageIcon } from "../icons";
 import { BodyWrapper } from "./BodyWrapper";
+import { BoxedIcon } from "./BoxedIcon";
 import { UserAvatar } from "./UserAvatar";
 
 export const MainLayout: React.FC<{ children?: React.ReactNode }> = ({
@@ -10,7 +13,22 @@ export const MainLayout: React.FC<{ children?: React.ReactNode }> = ({
       <div className="flex items-center w-full mt-2">
         <h3 className="flex flex-1 text-primary-100">Swipe</h3>
         <div className="flex gap-3">
-          <UserAvatar src="https://placekitten.com/200/200" size="md" />
+          <Link to={"/"}>
+            <BoxedIcon>
+              <HomeIcon />
+            </BoxedIcon>
+          </Link>
+          <Link to={"/notifications"}>
+            <BoxedIcon>
+              <Codicon name="notifications" />
+            </BoxedIcon>
+          </Link>
+          <Link to={"/messanger"}>
+            <BoxedIcon>
+              <MessageIcon />
+            </BoxedIcon>
+          </Link>
+          <UserAvatar src="https://placekitten.com/200/200" size="sm" />
         </div>
       </div>
       {children}
