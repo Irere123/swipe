4b50a006b4fc8c25507ff0cb81a9fb6a65e8ce17
irere123 <irere2020@outlook.com>
2022-08-27 21:34:57 +0200

    feat(api): added the authenticatication route

--- packages/api/src/routes/v1/user.ts
+++ packages/api/src/routes/v1/user.ts
@@ -1,9 +1,27 @@
 import { PrismaClient } from "@prisma/client";
 import { Router } from "express";
 import { __prod__ } from "../../constants";
+import { verify } from "jsonwebtoken";
+import { createTokens, RefreshTokenData } from "../../auth/createTokens";
 
 const router = Router();
 const prisma = new PrismaClient();
+
+router.get("/auth", async (req, res) => {
+  const { refreshToken } = req.body;
+
+  const verified = <RefreshTokenData>(
+    verify(refreshToken, process.env.REFRESH_TOKEN_SECRET)
+  );
+
+  const user = await prisma.user.findFirst({ where: { id: verified.userId } });
+
+  if (user) {
+    return res.json({ user, tokens: createTokens(user) });
+  }
+
+  return res.json({ ok: false, error: "not authenticated" });
+});
 
 router.get("/:id", async (req, res) => {
   const user = await prisma.user.findFirst({ where: { id: req.params.id } });
