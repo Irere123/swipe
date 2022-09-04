97c806014a4f96c15bc4982457e376720d097c5d
irere123 <irere2020@outlook.com>
2022-09-05 01:30:13 +0200

    finished setting up tailwindcss

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,8 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+import { createClient, Provider } from "urql";
+
+const client = createClient({
+  url: "http://localhost:4000/graphql",
+});
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Provider value={client}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
