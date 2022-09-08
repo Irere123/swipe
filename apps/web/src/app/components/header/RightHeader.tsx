e77a7aa55702e2488b20f9b692d40530cdbab6d2
irere123 <irere2020@outlook.com>
2022-09-08 15:14:35 +0200

    web: added unfinished modal component

--- apps/web/src/app/components/header/RightHeader.tsx
+++ apps/web/src/app/components/header/RightHeader.tsx
@@ -1,15 +1,24 @@
-import { Component } from "solid-js";
+import { Component, createSignal, onMount } from "solid-js";
 import { BoxedIcon } from "../BoxedIcon";
 import { Button } from "../Button";
 import { SolidMoreVert } from "../icons";
+import { Modal } from "../Modal";
 
 export const RightHeader: Component = () => {
+  const [openModal, setOpenModal] = createSignal(false);
+
   return (
     <div class="flex gap-2">
-      <Button>Login</Button>
+      <Button onClick={() => setOpenModal((v) => !v)}>Login</Button>
       <BoxedIcon>
         <SolidMoreVert width={24} height={27} />
       </BoxedIcon>
+      {openModal() && (
+        <Modal
+          isOpen={openModal()}
+          setOpenModal={() => setOpenModal((v) => !v)}
+        />
+      )}
     </div>
   );
 };
