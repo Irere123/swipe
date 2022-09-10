a702edff45f8e980d3c45352d1d4660b9b1d1258
irere123 <irere2020@outlook.com>
2022-09-10 12:30:56 +0200

    feat(api): setup  nexus

---
+++ apps/api/src/schema/user.ts
@@ -0,0 +1,27 @@
+import { objectType, queryType } from "nexus";
+import { prisma } from "../main";
+
+export const User = objectType({
+  name: "User",
+  definition(t) {
+    t.id("id"),
+      t.string("username"),
+      t.string("displayName"),
+      t.string("gender"),
+      t.string("location"),
+      t.string("birthday"),
+      t.boolean("online");
+  },
+});
+
+export const Users = queryType({
+  definition(t) {
+    t.list.field("users", {
+      type: "User",
+      resolve() {
+        const users = prisma.user.findMany();
+        return users;
+      },
+    });
+  },
+});
